8e589fb88956fb4212a2f461ada541d733ccdacf
Nicolás Valiconto <nvaliconto@outlook.com>
2023-04-25 18:21:27 -0300

    assigning http routes

--- router.go
+++ router.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type Router struct {
-	rules map[string]map[string]http.HandlerFunc
+type Router struct{
+	rules map[string]http.HandlerFunc
+
 }
 
 func NewRouter() *Router {
 	return &Router{
-		rules: make(map[string]map[string]http.HandlerFunc),
+		rules: make(map[string]http.HandlerFunc),
 	}
 }
 
+func (r *Router) FindHandler(path string) (http.HandlerFunc, bool){
+	handler, exist := r.rules[path]
+	return handler, exist
+}
+
+
+
 func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	handler, exist := r.FindHandler(request.URL.Path)
+
+	if !exist{
+		w.WriteHeader(http.StatusNotFound)
+		return 
+	}
+	handler(w, request)
 }

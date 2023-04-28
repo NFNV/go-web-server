f8084859222eb7731c9cd7eb27403642ca9a34a5
Nicolás Valiconto <nvaliconto@outlook.com>
2023-04-28 17:09:37 -0300

    answering with JSON

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,3 +14,30 @@ func HandleHome(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "API endpoint")
 }
 
+func PostRequest(w http.ResponseWriter, r *http.Request){
+	decoder := json.NewDecoder(r.Body)
+	var metadata Metadata
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request){
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJSON()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
